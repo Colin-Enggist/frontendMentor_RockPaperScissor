78e1b9029d5a780504c324946ea24eb507b0fa6a
Colin Enggist <99475091+Kokossnuss@users.noreply.github.com>
2022-06-10 11:56:06 +0200

    rules

--- modules/buttonHandlers/rules.js
+++ modules/buttonHandlers/rules.js
@@ -1,13 +1,11 @@
-import {data} from "../../main.js"
+import {data, ctx} from "../../main.js"
 
 export default function rules(){
     let rules = new Image()
     rules.src = data.images.rulesRPS.path
 
     rules.onload = ()=>{
-        let display = document.createElement("img")
-        display.appendChild(rules)
-        alert(display);
+        ctx.drawImage(rules, 252, 115);
     }
    
 }
