970e1d706effe3448d58974910247b719959e090
Colin Enggist <99475091+Kokossnuss@users.noreply.github.com>
2022-06-10 13:02:02 +0200

    rules

--- modules/buttonHandlers/rules.js
+++ modules/buttonHandlers/rules.js
@@ -1,11 +1,22 @@
-import {data, ctx} from "../../main.js"
+import {data, ctx, screen} from "../../main.js"
+import stepOne from "../stepOne.js";
 
 export default function rules(){
     let rules = new Image()
     rules.src = data.images.rulesRPS.path
 
     rules.onload = ()=>{
+        ctx.clearRect(0, 0, screen.width, screen.height);
+        ctx.beginPath();
+        ctx.rect(235, 100, 330, 230);
+        ctx.fillStyle = "white";
+        ctx.fill();
         ctx.drawImage(rules, 252, 115);
+        screen.addEventListener("click", ()=>{
+            ctx.clearRect(0, 0, screen.width, screen.height);
+            return stepOne();
+        })
+        
     }
    
 }
