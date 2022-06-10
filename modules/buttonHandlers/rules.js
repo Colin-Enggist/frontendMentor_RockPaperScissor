9e6d818583f165cb440f0978f7fd4b235d84673c
Colin Enggist <99475091+Kokossnuss@users.noreply.github.com>
2022-06-10 11:46:26 +0200

    now it works

--- modules/buttonHandlers/rules.js
+++ modules/buttonHandlers/rules.js
@@ -2,7 +2,7 @@ import {data} from "../../main.js"
 
 export default function rules(){
     let rules = new Image()
-    rules.src = data.images.rulesRPS
+    rules.src = data.images.rulesRPS.path
 
     rules.onload = ()=>{
         alert(rules);
